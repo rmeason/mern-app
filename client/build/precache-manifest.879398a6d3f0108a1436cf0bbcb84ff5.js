dd5ddb863d696dd19d1d66cfe0f194acbe353bb8
rmeason <rme15a@acu.edu>
2020-07-10 01:05:15 -0500

    initial commit

---
+++ client/build/precache-manifest.879398a6d3f0108a1436cf0bbcb84ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af78e3e622f4f5db238b",
+    "url": "/static/css/main.57d95243.chunk.css"
+  },
+  {
+    "revision": "af78e3e622f4f5db238b",
+    "url": "/static/js/main.36dc9dba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "2df56eef06b0e94b7c3d2673db92c674",
+    "url": "/index.html"
+  }
+];
